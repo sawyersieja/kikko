2dce28ad5db2e3367696f4074a760bec582ad381
sawyersieja <sawyersieja@protonmail.com>
2025-03-22 15:22:00 -0700

    refactor: Reorganized about->page.tsx to consist of sections of about components. - Added content for about->AboutSummary.tsx - Added content for about->Skills.tsx - Added content for about->Experience.tsx - Added content for about->CTA.tsx

--- src/app/about/AboutSummary.tsx
+++ src/app/about/AboutSummary.tsx
@@ -1,6 +1,8 @@
 const AboutSummary = () => {
     return(
         <section>
+            <h2 className="text-2xl font-semibold">Who I am</h2>
+            <p>I&apos;m a software engineer with a strong interest in cybersecurity, Web3, and secure application development. My work focuses on full-stack development, networking, encryption, and building scalable high-performance systems.</p>            
             
         </section>
     );
