7976516e4981f915de537e4c3754030d2ba1e5ea
sawyersieja <sawyersieja@protonmail.com>
2025-04-11 22:51:36 -0700

    fix: Netlify Forms support for Next.js App Router
    - Added hidden fallback HTML form for Netlify to detect form during build.
    - Resolved plugin error with @netlify/plugin-nextjs v5.
    - Cleaned up netlify.toml for compatability.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,6 +7,13 @@ const Contact = () => {
                 <h1 className="text-4xl font-bold mt-10 mb-6">Contact Me</h1>
                 <p className="text-gray-300 font-medium text-base mb-6">Fill out the form below to send me a message.</p>
 
+                {/* Static fallback form for Netlify */}
+                <form name="contact" method="POST" data-netlify="true" hidden>
+                    <input type="hidden" name="form-name" value="contact" />
+                    <input type="email" name="email" />
+                    <textarea name="message"></textarea>
+                </form>
+
                 <ContactForm />
             </>
     );
