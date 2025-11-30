f959e35357c2e0766a766858563f62ea8d61c287
sawyersieja <sawyersieja@protonmail.com>
2025-11-29 19:51:47 -0800

    fix: Hero backgroundImage to  and jpg to webp for image optimization and priority loading

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,11 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Hero = () => {
     return (
-        <section 
+        <section
             className="relative bg-cover bg-center bg-no-repeat flex-1 flex flex-col justify-center text-foreground text-center px-4"
-            style={{ backgroundImage: "url('/images/hero-waves.jpg')"}}
         >
+            <Image
+                src="/images/hero-waves-2560.webp" // You can keep this JPG for now!
+                alt=""
+                fill
+                priority
+                sizes="100vw"
+                className="object-cover -z-10"
+            />
             <div className="absolute inset-0 z-0 bg-white/60 dark:bg-black/30" />
 
             <div className="relative z-10 bg-gray-700/40 backdrop-blur-xs rounded-xl p-10 max-w-3xl mx-auto shadow-xl">
@@ -15,7 +23,7 @@ const Hero = () => {
                 <p className="mt-4 text-base sm:text-xl text-white drop-shadow-sm">
                     I build reliable, scalable, and secure software - always with the end user in mind.
                 </p>
-                <Link 
+                <Link
                     href="/projects"
                     className="mt-6 inline-block px-6 py-3 bg-primary hover-bg-accent hover:scale-[1.02] transition-all rounded-lg font-medium text-white shadow-md"
                 >
